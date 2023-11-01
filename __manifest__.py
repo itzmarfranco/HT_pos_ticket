5e8b6f6e339503a4b5dd9f83b578d59e153b34b2
Kirby <kairi.franco@analytects.com>
2023-11-01 00:16:47 -0600

    fixed manifest and added ticket mod file

--- __manifest__.py
+++ __manifest__.py
@@ -1,16 +1,15 @@
 {
     'name': 'POS Ticket Modification',
-    'version': '1.0',
-    'summary': 'Custom modification for the POS ticket',
-    'description': 'Add custom modifications to the POS ticket',
-    'author': 'Your Name',
+    'version': '10.0.0',
+    'summary': 'Modification for the POS ticket for HT',
+    'description': 'Add modifications to the POS ticket',
+    'author': 'Kairi Franco',
     'category': 'Point of Sale',
     'depends': ['point_of_sale'],
     'data': [
-        'views/pos_ticket_modification.xml',
     ],
     'qweb': [
-        'static/src/js/pos_ticket_modification.xml',
+        'static/src/xml/pos_ticket_modification.xml',
     ],
     'installable': True,
     'auto_install': False,
